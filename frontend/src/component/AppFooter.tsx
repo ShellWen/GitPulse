4a3009f58b41bae6488394ff093386a594e8e4f3
ShellWen <me@shellwen.com>
2024-11-03 22:16:48 +0800

    feat(frontend): add footer text when mock enabled

--- frontend/src/component/AppFooter.tsx
+++ frontend/src/component/AppFooter.tsx
@@ -6,7 +6,8 @@ const AppFooter = () => {
       <Footer className="items-center justify-center p-4">
         <aside className="grid-flow-col items-center">
           <span className="text-lg font-bold">GitPulse</span>
-          <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+          <p>Copyright © {new Date().getFullYear()} - All right reserved.</p>
+          {import.meta.env.VITE_ENABLE_MOCK && <p>Network Mock enabled.</p>}
         </aside>
       </Footer>
     </footer>
